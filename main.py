0d3eca23bfed2389dcbd322595b88239ccc447c7
CyberAvian <cyberavian025@gmail.com>
2021-02-13 20:38:51 -0500

    Regex to find and return articles

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
+import re
 import requests
 from bs4 import BeautifulSoup
 
-website = requests.get('https://www.darkreading.com/attacks-breaches.asp')
-soup = BeautifulSoup(website.content, 'html.parser')
+# patternObj = re.compile('^[https://]{1}[.]*[d-id]{1}$[\d]+')
+site = './darkreading/darkreading13022021.html'
+links = []
 
-print(soup.prettify())
+# website = requests.get('https://www.darkreading.com/attacks-breaches.asp')
+with open(site, 'rb') as site_content:
+    soup = BeautifulSoup(site_content, 'html.parser')
+
+# for element in soup.find_all('a'):
+#     href = element.get('href')
+#     if 'd-id' in href and '#msgs' not in href and href not in links:
+#         links.append(href)
+
+for match in soup.find_all('a', href=re.compile('^https://{1}.*d-id{1}/{1}\d+$')):
+    href = match.get('href')
+    if href not in links:
+        links.append(href)
+
+for link in enumerate(links, start=1):
+    print(f"{link[0]}. {link[1]}")
